1acda2ae4a869ab703e16f5f252061eed8b2314d
kotayam <ykomako4.9@gmail.com>
2025-08-25 19:59:51 -0400

    update contacts

--- frontend/src/Contacts.tsx
+++ frontend/src/Contacts.tsx
@@ -1,54 +1,34 @@
-export default function Contancts() {
+type Contact = { title: string; link: string; mail?: boolean };
+const contacts: Contact[] = [
+  { title: "School email", link: "kotayama@seas.upenn.edu", mail: true },
+  { title: "Personal email", link: "ykomako4.9@gmail.com", mail: true },
+  { title: "LinkedIn", link: "https://www.linkedin.com/in/kota-yamamoto/" },
+  { title: "GitHub", link: "https://github.com/kotayam" },
+];
+
+export default function Contacts() {
   return (
     <div className="mt-5">
       <h2 id="contacts" className="text-white text-xl mb-1">
         Contacts
       </h2>
-      <ul>
-        <li>
-          <p>
-            School Email:{" "}
-            <a
-              href="mailto:kotayama@seas.upenn.edu"
-              className="hover:underline"
-            >
-              kotayama@seas.upenn.edu
-            </a>
-          </p>
-        </li>
-        <li>
-          <p>
-            Personal Email:{" "}
-            <a href="mailto:ykomako4.9@gmail.com" className="hover:underline">
-              ykomako4.9@gmail.com
-            </a>
-          </p>
-        </li>
-        <li>
-          <p>
-            LinkedIn:{" "}
-            <a
-              href="https://www.linkedin.com/in/kota-yamamoto/"
-              target="_blank"
-              className="hover:underline"
-            >
-              https://www.linkedin.com/in/kota-yamamoto/
-            </a>
-          </p>
-        </li>
-        <li>
-          <p>
-            GitHub:{" "}
-            <a
-              href="https://github.com/kotayam"
-              target="_blank"
-              className="hover:underline"
-            >
-              https://github.com/kotayam
-            </a>
-          </p>
-        </li>
-      </ul>
+      <div className="flex-col gap-1">
+        {contacts.map((c) => (
+          <div className="flex items-start gap-1">
+            <span>•</span>
+            <p>
+              {c.title}:{" "}
+              <a
+                href={c.mail ? `mailto:${c.link}` : c.link}
+                target="_blank"
+                className="hover:underline"
+              >
+                {c.link}
+              </a>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
