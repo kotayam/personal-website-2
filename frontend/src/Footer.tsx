8b3644beb331633f609167f3fce3b8d322a5e4ae
kotayam <ykomako4.9@gmail.com>
2025-08-24 22:12:42 -0400

    format

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -2,7 +2,10 @@ export default function Footer() {
   return (
     <div className="flex justify-center mt-5">
       <div className="max-w-[600px] min-w-[350px] p-4 text-center">
-        <p>This website was built by Kota Yamamoto using Typescript, React, Vite, and Tailwind CSS.</p>
+        <p>
+          This website was built by Kota Yamamoto using Typescript, React, Vite,
+          and Tailwind CSS.
+        </p>
       </div>
     </div>
   );
